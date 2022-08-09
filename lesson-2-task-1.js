8b2e03e8a2b959923c83023f8fd287c260e369cd
Diplomat999 <mmmaxxx1987@gmail.com>
2022-08-09 23:11:33 +0300

    task-2

--- lesson-2-task-1.js
+++ lesson-2-task-1.js
@@ -1,17 +1,17 @@
 // Lesson 2, task 1
 
-const name = prompt("What is your name?");
-const userAge = +prompt("Howe old are you?");
+const userName = prompt("What is your name?");
+const userAge = +prompt("How old are you?");
 
 if (userAge < 18) {
   alert("You are not allowed to visit this website");
 } else if (userAge >= 18 && userAge <= 22) {
   const question = confirm("Are you sure you want to continue?");
   if (question) {
-    alert("Welcome, " + name + ".");
+    alert("Welcome, " + userName + ".");
   } else {
     alert("You are not allowed to visit this website.");
   }
 } else {
-  alert(`Welcome, ${name}.`);
+  alert(`Welcome, ${userName}.`);
 }
---
+++ lesson-2-task-2.js
@@ -0,0 +1,27 @@
+// Lesson 2, task 2
+
+let userName = prompt("What is your name?");
+let userAge = +prompt("How old are you?");
+
+while (userName == "" || userName == null || !isNaN(+userName)) {
+  alert("Incorrected information");
+  userName = prompt("Please enter your name!");
+}
+
+while (userAge == '' || isNaN(userAge)) {
+    alert("Incorrected information");
+userAge = +prompt('Please enter your age!')
+}
+
+if (userAge < 18) {
+  alert("You are not allowed to visit this website");
+} else if (userAge >= 18 && userAge <= 22) {
+  const question = confirm("Are you sure you want to continue?");
+  if (question) {
+    alert("Welcome, " + userName + ".");
+  } else {
+    alert("You are not allowed to visit this website.");
+  }
+} else {
+  alert(`Welcome, ${userName}.`);
+}
