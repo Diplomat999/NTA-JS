6b2ad2184076ab6693b48e96eecad63b89727c1d
Diplomat999 <mmmaxxx1987@gmail.com>
2022-08-20 21:55:30 +0300

    Lesson5 Home work

---
+++ HomeWorks/Lesson6-Масиви/app.js
@@ -0,0 +1,11 @@
+// const names = ["Max", "Ivan", "Vlad", "Oksana", "Oleksandr"];
+
+// // delete names[names.indexOf('Vlad')]
+// let newNames = names.splice(2, 1);
+
+// console.log(names);
+
+// let text = "my name is Oksana";
+
+// let newText = text.split(" ");
+// console.log(newText);
