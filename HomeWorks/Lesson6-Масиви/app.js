153830e191098e1315f6b214453f4c71a8307cbd
Diplomat999 <mmmaxxx1987@gmail.com>
2022-08-21 20:51:00 +0300

    lesson6 Home work

--- HomeWorks/Lesson6-Масиви/app.js
+++ HomeWorks/Lesson6-Масиви/app.js
@@ -1,11 +1,38 @@
-// const names = ["Max", "Ivan", "Vlad", "Oksana", "Oleksandr"];
+// Реализовать функцию фильтра массива по указанному типу данных.
 
-// // delete names[names.indexOf('Vlad')]
-// let newNames = names.splice(2, 1);
+// Технические требования:
 
-// console.log(names);
+// Написать функцию filterBy(), которая будет принимать в себя 2 аргумента. Первый аргумент - массив, который будет содержать в себе любые данные, второй аргумент - тип данных.
+// Функция должна вернуть новый массив, который будет содержать в себе все данные, которые были переданы в аргумент, за исключением тех, тип которых был передан вторым аргументом. То есть, если передать массив ['hello', 'world', 23, '23', null], и вторым аргументом передать 'string', то функция вернет массив [23, null].
 
-// let text = "my name is Oksana";
 
-// let newText = text.split(" ");
-// console.log(newText);
+const arr = ["Max", "Ivan", 35, null, 42, "Jurii", 56];
+const test = ['Nick', 36, 'Sergiy', 19];
+
+
+function filterBy(arr, number) {
+  return function (item, index, array) {
+    return isNaN(item);
+  };
+}
+
+function filterByString(arr, string) {
+    return function (item, index, array) {
+      return !isNaN(item);
+    };
+  }
+
+console.log(arr.filter(filterBy()));
+console.log(test.filter(filterBy()));
+console.log(arr.filter(filterByString()));
+console.log(test.filter(filterByString()));
+
+// або
+
+// console.log(arr.filter((item, index, array) => {
+// return isNaN(item);
+// }));
+
+// console.log(arr.filter((item, index, array) => {
+// return !isNaN(item);
+// }));
